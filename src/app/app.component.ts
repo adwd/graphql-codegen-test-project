e3478d6e2971964683bb599baccb88d1107ef3b5
adwd <ecdt.mn@gmail.com>
2020-03-15 22:30:18 +0900

    add graphql-codegen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, Optional, ChangeDetectorRef } from '@angular/core';
+import gql from 'graphql-tag';
+import { LaunchesGQL } from 'src/generated/graphql';
+
+const query = gql`
+  query Launches {
+    launchesPast(limit: 10) {
+      mission_name
+    }
+  }
+`;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'graphql-codegen-test-project';
+  result = '';
+  @Input() foo: any; // this @Input decorator does not affect
+
+  constructor(private launches: LaunchesGQL, @Optional() cd: ChangeDetectorRef) { // this @Optional decorator causes error
+    launches
+      .fetch()
+      .subscribe(res => (this.result = JSON.stringify(res)));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: 'https://api.spacex.land/graphql/'
+          })
+        };
+      },
+      deps: [HttpLink]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
